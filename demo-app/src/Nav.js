77ab9d6d7206fd34409943fcf4d4e9f3afe1afa5
Matias Aguayo <matias.aguayo@usm.cl>
2020-12-12 19:25:07 -0300

    comenzando a editar la pagina de tiendas(pcexpress)

--- demo-app/src/Nav.js
+++ demo-app/src/Nav.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css';
 import { Link } from "react-router-dom";
-import logo from "./bambamiconopng.png"
+import logo from "./bambamiconopng.png";
 
 function Nav() {
   const navStyle = {
--- demo-app/src/Tiendas.js
+++ demo-app/src/Tiendas.js
@@ -1,10 +1,21 @@
 import React from "react";
 import './App.css';
+import pcexpress from "./pcexpress.png";
 
 function Tiendas() {
   return (
     <div>
         <h1>Tiendas Page</h1>
+        <div classname="pcexpress">
+          <img src={pcexpress} 
+          width="200" 
+          height="100">
+        </img>
+        <h2>PcExpress</h2>
+        </div>
+        <div>
+          A pesar de que la seccion dice tiendas, por ahora solo sera esta tienda, ya que te tiene una mayor vericidad de las reputaciones y comentarios de esta.          
+        </div>
     </div>
   );
 }
