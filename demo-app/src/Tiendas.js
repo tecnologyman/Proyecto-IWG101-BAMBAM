5699c437907d940444bb26445b5d0bcb45d48d0f
Matias Aguayo <matias.aguayo@usm.cl>
2020-12-23 13:23:41 -0300

    mejora de la visual de la seccion tiendas

--- demo-app/src/Tiendas.js
+++ demo-app/src/Tiendas.js
@@ -6,28 +6,27 @@ import DataTiendas from "./tiendas.json";
 function Tiendas() {
   return (
     <div>
-      <div classname="pcexpress">
+      <ul class="cuadro">
         <a title="PCExpress" href="https://tienda.pc-express.cl" target="_blank"><img src={pcexpress} alt="PCExpress" height="216" width="384"/></a>
-      </div>
-      <div> 
         {DataTiendas.map((data, index)=>{
           return <div>
             <u><h1>{data.name}</h1></u>
             <h2 align="center">Ranking: {data.ranking}</h2>
-            <div align="left" margin="1"> <h4>
-              {data.comment1}
-              <p>{data.comment2}</p>
-              <p>{data.comment3}</p>
-              <p>{data.comment4}</p>
-              <p>{data.comment5}</p>
-              <p>{data.comment6}</p>
-              <p>{data.comment7}</p>
-              <p>{data.comment8}</p>
-              <p>{data.comment9}</p> 
-            </h4></div> 
+            <h1>Comentarios:</h1>
+            <div class="comentarios">
+              <h4>{data.comment1}</h4><h1></h1>
+              <h4>{data.comment2}</h4><h1></h1>
+              <h4>{data.comment3}</h4><h1></h1>
+              <h4>{data.comment4}</h4><h1></h1>
+              <h4>{data.comment5}</h4><h1></h1>
+              <h4>{data.comment6}</h4><h1></h1>
+              <h4>{data.comment7}</h4><h1></h1>
+              <h4>{data.comment8}</h4><h1></h1>
+              <h4>{data.comment9}</h4><h1></h1>
+            </div>
         </div>
        })}
-     </div>
+     </ul>
     </div>
   );
 }
