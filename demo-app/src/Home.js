51028b65af26a31bf646d5cc618b4148e39fafa4
Matias Aguayo <matias.aguayo@usm.cl>
2020-12-23 15:16:46 -0300

    .

--- demo-app/src/Home.js
+++ demo-app/src/Home.js
@@ -8,14 +8,14 @@ function Home() {
     <div class="inicio">
       <img src={logo} width="150" height="100"></img>
       <h1>¡Hola!, Bienvenido a Feedback Store´s o tambien llamada BAMBAM.</h1>
-      <h2>¿Eres Nuevo por aquí?. Pues te explicaré como funcionará esta web.</h2> <br/>
+      <i> <h2>¿Eres Nuevo por aquí?. Pues te explicaré como funcionará esta web.</h2> <br/>
       <h3>Para comenzar, aquí encontrarás tiendas de hardware de todo Chile, permitiendote conocer sus </h3>
       <h3>defectos como virtudes, y así tener unas desición clara a la hora de comprar en dichas tiendas</h3> <br/>
       <h4>En la barra de navegacion superior puedes observar las secciones "home", "tiendas" y "about". En este momento estas en "home"</h4>
-      <h4>En la seccion tiendas puedes observar todas las tiendas que tendremos en la pagina, pero por ahora por la facilidad y confiabilidad solo estaran los comentarios sobre la pagina pcexpress, todo esto en la seccion "tiendas"</h4>
+      <h4>En la seccion tiendas puedes observar todas las tiendas que tendremos en la pagina, pero por ahora por la facilidad y confiabilidad solo estaran los comentarios sobre la pagina pcexpress, todo esto en la seccion "tiendas"</h4> </i>
       <div class="pc">
-        <h2>Esta Tienda es PcExpress</h2>
-        <img src={intro} width="1100" height="850" class="pce"></img>
+      <u><h2>Esta Tienda es PcExpress</h2></u>
+        <a title="PcExpress" href="https://tienda.pc-express.cl" target="_blank"><img src={intro} alt="PcExpress" height="850" width="1100" class="pce"/></a>
       </div>
     </div>
   );
