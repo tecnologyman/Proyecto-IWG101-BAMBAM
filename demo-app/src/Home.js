f4352cf2ca1b5d09339745b43e34c8e9d195d5d1
Matias Aguayo <matias.aguayo@usm.cl>
2021-01-06 13:26:36 -0300

    .

--- demo-app/src/Home.js
+++ demo-app/src/Home.js
@@ -10,7 +10,7 @@ function Home() {
       <h1>¡Hola!, Bienvenido a Feedback Store´s o tambien llamada BAMBAM.</h1>
       <i> <h2>¿Eres Nuevo por aquí?. Pues te explicaré como funcionará esta web.</h2> <br/>
       <h3>Para comenzar, aquí encontrarás tiendas de hardware de todo Chile, permitiendote conocer sus </h3>
-      <h3>defectos como virtudes, y así tener unas desición clara a la hora de comprar en dichas tiendas</h3> <br/>
+      <h3>defectos como virtudes, y así tener una desición mas clara a la hora de comprar en dichas tiendas</h3> <br/>
       <h4>En la barra de navegacion superior puedes observar las secciones "home", "tiendas" y "about". En este momento estas en "home"</h4>
       <h4>En la seccion tiendas puedes observar todas las tiendas que tendremos en la pagina, pero por ahora por la facilidad y confiabilidad solo estaran los comentarios sobre la pagina pcexpress, todo esto en la seccion "tiendas"</h4> </i>
       <div class="pc">
