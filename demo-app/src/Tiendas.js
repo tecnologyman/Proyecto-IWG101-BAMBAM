7f169c32504dfe02abb86baecfa4ea940306e9e2
Matias Aguayo <matias.aguayo@usm.cl>
2021-01-06 13:14:09 -0300

    mas comentarios y validacion

--- demo-app/src/Tiendas.js
+++ demo-app/src/Tiendas.js
@@ -2,6 +2,7 @@ import React from "react";
 import './App.css';
 import pcexpress from "./pcexpress.png";
 import DataTiendas from "./tiendas.json";
+import "./texto.txt"
 
 function Tiendas() {
   return (
@@ -23,6 +24,12 @@ function Tiendas() {
               <h4>{data.comment7}</h4>
               <h4>{data.comment8}</h4>
               <h4>{data.comment9}</h4>
+              <h4>{data.comment10}</h4>
+              <h4>{data.comment11}</h4>
+              <h4>{data.comment12}</h4>
+              <h4>{data.comment13}</h4>
+              <h4>{data.comment14}</h4>
+              <h4>{data.comment15}</h4>
             </div>
         </div>
        })}
